63c9a2b9e25181dd19ce9c1d5e21afc55565826d
Javi H <javihernant@gmail.com>
2023-09-22 00:02:40 +0200

    clean old code

--- src/main.c
+++ src/main.c
@@ -86,52 +86,6 @@ int	key_release(int keycode, t_keys *keys)
 	return 0;
 }
 
-// int	handle_key(int keycode, void *sth)
-// {
-// 	(void) sth;
-// 	if (keycode == XK_a)
-// 	{
-// 		printf("a\n");
-// 		stepx = dirx * cos(-1.5707) - diry * sin(-1.5707);
-// 		stepy = dirx * sin(-1.5707) + diry * cos(-1.5707);
-
-// 		pressed = 1;
-// 	}
-// 	else if (keycode == XK_w)
-// 	{
-// 		printf("w\n");
-// 		stepx = dirx;
-// 		stepy = diry;
-// 		// stepx = 0;
-// 		pressed = 1;
-// 	}
-// 	else if (keycode == XK_s)
-// 	{
-// 		stepx = -dirx;
-// 		stepy = -diry;
-// 		pressed = 1;
-// 	}
-// 	else if (keycode == XK_d)
-// 	{
-// 		printf("d\n");
-// 		stepx = dirx * cos(1.5707) - diry * sin(1.5707);
-// 		stepy = dirx * sin(1.5707) + diry * cos(1.5707);
-// 		pressed = 1;
-
-// 	}
-// 	else if (keycode == XK_Left)
-// 	{
-// 		printf("<\n");
-// 		pressed = 2;
-// 	}
-// 	else if (keycode == XK_Right)
-// 	{
-// 		printf(">\n");
-// 		pressed = 3;
-// 	}
-// 	return 0;
-// }
-
 void	draw_map(t_mlxconf *conf)
 {
 	int pixx = posx * WIDTH / MAP_W;
@@ -167,56 +121,6 @@ void	draw_map(t_mlxconf *conf)
 		y++;
 	}
 }
-
-// void	mod_pos()
-// {
-// 	start = end;
-// 	end = clock();
-// 	double fps = (double) (end - start) / CLOCKS_PER_SEC;
-
-// 	double spmov = fps * 15;
-// 	if (pressed == 1)
-// 	{
-// 		double newX = posx + stepx * spmov;
-// 		double newY = posy + stepy * spmov;
-// 		// printf("%f , %f\n", newX, newY);
-// 		if (newX >= 0 && newX < MAP_W && newY >= 0 && newY < MAP_H)
-// 		{
-
-// 			if (worldMap[(int)newY][(int)newX] == 0)
-// 			{
-// 				posx = newX;
-// 				posy = newY;
-// 			}
-// 		}
-// 		pressed = 0;
-// 	}
-// 	double rotspeed = fps * 5;
-// 	if (pressed == 2)
-// 	{
-// 		rotspeed = -rotspeed;
-// 		double olddirx = dirx;
-// 		dirx = dirx * cos(rotspeed) - diry * sin(rotspeed);
-// 		diry = olddirx * sin(rotspeed) + diry * cos(rotspeed);
-
-// 		double oldplanex = planex;
-//      	planex = planex * cos(rotspeed) - planey * sin(rotspeed);
-//       	planey = oldplanex * sin(rotspeed) + planey * cos(rotspeed);
-// 		pressed = 0;
-// 	}
-// 	if (pressed == 3)
-// 	{
-// 		double olddirx = dirx;
-// 		dirx = dirx * cos(rotspeed) - diry * sin(rotspeed);
-// 		diry = olddirx * sin(rotspeed) + diry * cos(rotspeed);
-
-// 		double oldplanex = planex;
-//      	planex = planex * cos(rotspeed) - planey * sin(rotspeed);
-//       	planey = oldplanex * sin(rotspeed) + planey * cos(rotspeed);
-// 		pressed = 0;
-// 	}
-// 	// printf("%f\n", spmov);
-// }
 
 void rotate_dir(double rotspeed)
 {
@@ -288,103 +192,6 @@ void	mod_pos(t_keys *keys)
 
 }
 
-// void	print_rays(t_mlxconf *conf)
-// {
-
-// 	int	x = 0;
-// 	int pixx0 = posx * (double)WIDTH/ MAP_W;
-// 	int pixy0 = posy * (double)HEIGHT/ MAP_H;
-// 	// if (1)
-// 	// if (1)
-// 	while (x < WIDTH)
-// 	{
-// 		double camx = 2 * (double) x / WIDTH - 1;
-// 		double ray = camx * 1;
-
-// 		double ratiox =  (dirx + ray*planex) == 0 ? 999999 : 1/fabs(dirx + ray*planex); //TODO: if div is 0
-// 		double ratioy = (diry + ray*planey) == 0 ? 999999 : 1/fabs(diry + ray*planey);
-// 		double distx;
-// 		double disty;
-// 		// double obsx = posx + dirx;
-// 		// double obsy = posy + diry;
-// 		double sx;
-// 		double	sy;
-// 		if (dirx + ray*planex < 0)
-// 		{
-// 			distx = (posx - floor(posx)) * ratiox;
-// 			sx = -1;
-// 		}
-// 		else
-// 		{
-// 			distx = (ceil(posx) - posx) * ratiox;
-// 			sx = 1;
-// 		}
-// 		if (diry + ray*planey < 0)
-// 		{
-// 			disty = (posy - floor(posy)) * ratioy;
-// 			sy = -1;
-// 		}
-// 		else
-// 		{
-// 			disty = (ceil(posy) - posy) * ratioy;
-// 			sy = 1;
-// 		}
-// 		int hit = 0;
-// 		int side;
-// 		double obsx = (int) posx;
-// 		double obsy = (int) posy;
-// 		// double raydirx = dirx + planex*ray;
-// 		// double raydiry = diry + planey*ray;
-// 		while(!hit)
-// 		{
-// 			if (distx < disty)
-// 			{
-// 				obsx += sx;
-// 				distx += ratiox;
-// 				side = 0;
-// 			}
-// 			else
-// 			{
-// 				obsy += sy;
-// 				disty += ratioy;
-// 				side = 1;
-// 			}
-// 			if (worldMap[(int)obsy][(int)obsx] != 0)
-// 			{
-// 				if (side == 0)
-// 				{
-// 					obsx = posx + (dirx + planex*ray) * (distx - ratiox);
-// 					obsy = posy + (diry + planey*ray) * (distx - ratiox);
-// 				}
-// 				else
-// 				{
-// 					obsx = posx + (dirx + planex*ray) * (disty - ratioy);
-// 					obsy = posy + (diry + planey*ray) * (disty - ratioy);
-
-// 				}
-// 				break;
-// 			}
-// 		}
-
-
-// 		int pixx1 = obsx * (double) WIDTH / (double) MAP_W;
-// 		int pixy1 = obsy * (double) HEIGHT / (double) MAP_H;
-
-// 		if (pixx1 < 0)
-// 			pixx1 = 0;
-// 		if (pixx1 >= WIDTH)
-// 			pixx1 = WIDTH - 1;
-// 		// int pixy1 = 0;
-// 		if (pixy1 < 0)
-// 			pixy1 = 0;
-// 		if (pixy1 >= HEIGHT)
-// 			pixy1 = HEIGHT - 1;
-// 		ft_draw_line(pixx0, pixy0, pixx1, pixy1, 0x00ff00, conf);
-
-// 		x++;
-// 	}
-// }
-
 void	obstacle_dist()
 {
 
@@ -395,8 +202,6 @@ void	obstacle_dist()
 		double ratioy = (diry + rays[x]*planey) == 0 ? 999999 : 1/fabs(diry + rays[x]*planey);
 		double distx;
 		double disty;
-		// double obsx = posx + dirx;
-		// double obsy = posy + diry;
 		double sx;
 		double	sy;
 		if (dirx + rays[x]*planex < 0)
@@ -422,8 +227,7 @@ void	obstacle_dist()
 		int hit = 0;
 		double obsx = (int) posx;
 		double obsy = (int) posy;
-		// double raydirx = dirx + planex*ray;
-		// double raydiry = diry + planey*ray;
+
 		while(!hit)
 		{
 			if (distx < disty)
